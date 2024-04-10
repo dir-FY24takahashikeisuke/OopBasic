4f9a46ad62104bab813f021188d18233ceec2cde
dir-FY24takahashikeisuke <KeisukeTakahashi(@Dirbato-1402x>
2024-04-10 11:48:44 +0900

    コミット

--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -17,12 +17,37 @@ public class ObjectSample {
         
         //　インスタンスの作成
         Department devDepartment = new Department("開発部", "yy", 0);
-        Employee engineer = new Employee("田中", devDepartment, "一般社員", 88);
+        Engineer engineer = new Engineer("田中", devDepartment, "一般社員", 88, "Java");
         
         
        //　インスタンスメソッドの呼び出し
         engineer.report();
         engineer.joinMeeting();
+        engineer.developSoftware();
+        
+        //ポリモーフィズムの確認
+        Employee projectManager = new Engineer("佐藤", devDepartment, "PM", 99, "Java"); 
+        
+        //インスタンスメソッドの呼び出し
+        projectManager.report();
+        projectManager.joinMeeting();
+        if(projectManager instanceof Engineer) {
+            // developSoftwareメソッドを呼び出す
+            ((Engineer)projectManager).developSoftware();
+        }
+        
+        //１行空ける
+        System.out.println("");
+        
+        //アルバイトインスタンスの作成
+        ParttimeWorker parttimeWorker = new ParttimeWorker("太田", devDepartment);
+        
+        //実装したメソッドの呼び出し
+        ((Workable)employee).work();
+        ((Workable)engineer).work();
+        ((Workable)projectManager).work();
+        ((Workable)parttimeWorker).work();
+        
 
     }
 
